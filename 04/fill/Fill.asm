81ebf002bf27356ebb16843866121ac854da07af
Yang Li-Yi <yangliyi1228@gmail.com>
2017-09-16 22:01:37 +0800

    project04

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -2,13 +2,67 @@
 // and the book "The Elements of Computing Systems"
 // by Nisan and Schocken, MIT Press.
 // File name: projects/04/Fill.asm
-
+//
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+// LOOP:
+//   if key input > 0
+//     set color to -1
+//   else
+//     set color = 0
+//
+//   Write whole screen with above color
+//
+//   goto LOOP
+
+(LOOP)
+  @SCREEN
+  D=A
+  @screenaddress
+  M=D
+
+  @KBD
+  D=M
+  @SETBLACK
+  D;JNE // Jump and set color to black if not 0 (key pressed)
+
+  @color
+  M=0	// SETWHITE
+
+  @WRITECOLOR
+  0;JMP
+
+(SETBLACK)
+  @color
+  M=-1	// Set to black (1111111111111111)
+  @WRITECOLOR
+  0;JMP
+
+(WRITECOLOR)
+  @color
+  D=M
+
+  @screenaddress
+  A=M
+  M=D
+
+  @screenaddress
+  D=M+1	// move to next set
+  @KBD
+  D=A-D	// next pixel address - keybard address
+
+  @screenaddress
+  M=M+1	// move to next set
+  A=M
+
+  @WRITECOLOR
+  D;JGT	// If D = 0, it means we finished writing screen memory map and moved to keyboard memory map
+
+  @LOOP
+  0;JMP // Jump back to top of the loop
--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -6,4 +6,64 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// i = 1
+// n = R1
+// number = R0
+// sum = 0
+// R2 = 0
+
+// LOOP:
+//   if i > n goto STOP
+//   sum = sum + number
+//   i = i + 1
+//   goto LOOP
+// STOP:
+//   R2 = sum
+
+  @i
+  M=1
+
+  @R1
+  D=M
+  @n
+  M=D
+
+  @R0
+  D=M
+  @number
+  M=D
+
+  @R2
+  M=0 // Set R2 to 0 at the beginning since it's required in test cases...
+
+  @sum
+  M=0
+
+(LOOP)
+  @i
+  D=M
+  @n
+  D=D-M
+  @STOP
+  D;JGT // if i > n goto STOP
+
+  @number
+  D=M
+
+  @sum
+  M=M+D // sum = sum + number
+
+  @i
+  M=M+1 // i = i + 1
+  @LOOP
+  0;JMP
+
+(STOP)
+  @sum
+  D=M
+  @R2
+  M=D // R2 = sum
+
+(END)
+  @END
+  0;JMP
